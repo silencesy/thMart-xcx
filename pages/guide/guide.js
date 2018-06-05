1b929eaef1e943ae59cb86d851c2367b23d18f04
Your Name <you@example.com>
2018-06-05 16:42:12 +0800

    1

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -3,13 +3,52 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    classification: [],
+    articleList: [],
+    activeCategoryId: 1,
+    p: 0,
+    pageSize: 10,
+    totalPages: -1,
+    scrollTop: 0
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+    var that = this;
+    that.getArticleList();
+  },
+  tabClick: function (e) {
+    // 点击不是当前tab切换
+    if (this.data.activeCategoryId != e.currentTarget.id) {
+      this.setData({
+        articleList: [],
+        activeCategoryId: e.currentTarget.id,
+        p: 0,
+        totalPages: -1,
+        scrollTop: 0
+      });
+      // 请求数据
+      this.getArticleList();
+    }
+  },
+  // 获取数据
+  getArticleList: function () {
+    var that = this;
+    util.request('get', 'Api/Article/getList', {
+      cat_id: that.data.activeCategoryId,
+      p: that.data.p++,
+      pageSize: that.data.pageSize
+    }, function (res) {
+      res.data.data.cat.shift();
+      that.setData({
+        classification: res.data.data.cat,
+        articleList: res.data.data.articles
+      });
+      console.log(res);
+    });
+  },
+  // 上拉加载数据
+  lower: function () {
+    this.getArticleList(true);
   }
-})
+
+
+});
